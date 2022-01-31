b1031f5b1670aa97a716597d28aa887ec567ec95
MarcusKri <marcus-kristoffersen@outlook.com>
2022-01-31 15:47:27 +0100

    Add prettier

--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {ListMovies, NewMovieForm} from "../application";
-import {Simulate} from "react-dom/test-utils";
+import { ListMovies, NewMovieForm } from "../application";
+import { Simulate } from "react-dom/test-utils";
 
 describe("movies application", () => {
-    
-    it("shows movie list", () => {
-        const element = document.createElement("div");
-        React.createElement("ListMovies")
-        ReactDOM.render(<ListMovies />, element);
-        expect(element.querySelector("h1").innerHTML)
-            .toEqual("List movies")
-        expect(element.innerHTML).toMatchSnapshot();
+  it("shows movie list", () => {
+    const element = document.createElement("div");
+    React.createElement("ListMovies");
+    ReactDOM.render(<ListMovies />, element);
+    expect(element.querySelector("h1").innerHTML).toEqual("List movies");
+    expect(element.innerHTML).toMatchSnapshot();
+  });
+
+  it("show new movie form", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(<NewMovieForm />, element);
+    expect(element.innerHTML).toMatchSnapshot();
+  });
+
+  it("submit new movie", () => {
+    const onAddMovie = jest.fn();
+
+    const element = document.createElement("div");
+    ReactDOM.render(<NewMovieForm onAddMovie={onAddMovie} />, element);
+
+    Simulate.change(element.querySelector("[data-testid=title]"), {
+      target: { value: "Movie 1" },
     });
+    Simulate.change(element.querySelector("[data-testid=year]"), {
+      target: { value: "2022" },
+    });
+    Simulate.submit(element.querySelector("form"));
 
-    it("show new movie form",() => {
-        const element = document.createElement("div");
-        ReactDOM.render(<NewMovieForm />, element);
-        expect(element.innerHTML).toMatchSnapshot();
-    })
-
-    it("submit new movie",() => {
-        const onAddMovie = jest.fn();
-        
-        const element = document.createElement("div");
-        ReactDOM.render(<NewMovieForm onAddMovie={onAddMovie}/>, element);
-
-        Simulate.change(element.querySelector("[data-testid=title]"), {target: {value: "Movie 1"}});
-        Simulate.change(element.querySelector("[data-testid=year]"), {target: {value: "2022"}});
-        Simulate.submit(element.querySelector("form"));
-
-        expect(onAddMovie).toHaveBeenLastCalledWith({
-            title: "Movie 1", year: "2022", plot: ""
-        })
-    })                                                
-
-})
+    expect(onAddMovie).toHaveBeenLastCalledWith({
+      title: "Movie 1",
+      year: "2022",
+      plot: "",
+    });
+  });
+});
